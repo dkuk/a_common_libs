726755b521db27f54c86d5653993d09922f7840a
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2014-01-31 17:07:38 +1100

    #12756 done. LightView removed. BS-modals extended with dynamic window generation function

--- assets/javascripts/bootstrap.modal.ext.js
+++ assets/javascripts/bootstrap.modal.ext.js
@@ -22,7 +22,7 @@ function resize_bs_modal(obj){
     $(obj).css('width', px_width);
   }
   else if( $(obj).attr('data-min-width') !== undefined ) {
-    $(obj).css('margin-left', 0);    
+    $(obj).css('margin-left', 0);
     $(obj).css('width', 'auto');
 
     $(obj).css('min-width', $(obj).attr('data-min-width'));
@@ -32,7 +32,7 @@ function resize_bs_modal(obj){
 
 
   var modal_body = $(obj).find('.modal-body')
-  if( $(obj).attr('data-height') !== undefined ){    
+  if( $(obj).attr('data-height') !== undefined ){
     var ext_height = 0;
     if( $(obj).find('.modal-footer').length == 1 ) {
       ext_height += $(obj).find('.modal-footer').outerHeight()
@@ -52,7 +52,7 @@ function resize_bs_modal(obj){
     var top = (w_height - px_height) / 2;
     $(obj).css('top', top);
     modal_body.css('max-height', px_height-ext_height-paddings_h);
-    
+
 
     // if(modal_body.height() < modal_height-ext_height) {
       // проблемы, когда контент в окне изменяется. Нельзя вызвать циклический ресайз!
@@ -69,34 +69,60 @@ function resize_bs_modal(obj){
 
 $(document).ready(function(){
 
-  $('.modal').on('show', function(){
+  $(document.body).on('show', '.modal', function () {
     resize_bs_modal(this);
   });
 
-  $(document.body).on('click','[data-toggle=modal]', function() {
-    if( $(this).attr('data-target') !== undefined ) {
-      if( $(this).attr('data-modal-height') !== undefined ) {
-        // todo: make for href='#modalId'
-        $($(this).attr('data-target')).attr('data-height',$(this).attr('data-modal-height'));
-      }
-  
-      if( $(this).attr('data-modal-width') !== undefined ) {
-        // todo: make for href='#modalId'
-        $($(this).attr('data-target')).attr('data-width',$(this).attr('data-modal-width'));
-      }
-  
-      if( $(this).attr('data-modal-min-width') !== undefined ) {
-        // todo: make for href='#modalId'
-        $($(this).attr('data-target')).attr('data-min-width',$(this).attr('data-modal-min-width'));
-      }
-    }
-
+  $(document.body).on('ajaxstop', '.modal-body', function () {
+    resize_bs_modal(this);
   });
 
   $(window).on('resize', function(){
     $('.modal').each(function(){
       resize_bs_modal(this);
     })
+  });
+
+  $(document.body).on('hide', '.modal', function () {
+    if (this.id == 'bs-modal-dynamic')
+      $(this).remove();
+  });
+
+  $(document.body).on('click','[data-toggle=modal]', function() {
+    var modal_w;
+    if ( typeof $(this).attr('data-target') == 'undefined' || $($(this).attr('data-target')).length < 1) {
+      $('#bs-modal-dynamic').remove();
+      var close_label = (typeof $(this).attr('data-close-label') != 'undefined') ? $(this).attr('data-close-label') : 'Close';
+      $('<div id="bs-modal-dynamic" class="modal I fade" tabindex="-1" role="dialog" aria-hidden="true" style="z-index: 1061;">'+
+           '<div class="modal-body"></div>'+
+           '<div class="modal-footer">'+
+             '<button class="" data-dismiss="modal" aria-hidden="true" style="vertical-align: middle;">'+close_label+'</button>'+
+           '</div>'+
+         '</div>').appendTo(document.body);
+
+      modal_w = $('#bs-modal-dynamic');
+      $(this).attr('data-target','#bs-modal-dynamic')
+    } else {
+      modal_w = $($(this).attr('data-target'));
+    }
+
+    if ( typeof $(this).attr('data-modal-height') != 'undefined' ) {
+      // todo: make for href='#modalId'
+      modal_w.attr('data-height',$(this).attr('data-modal-height'));
+    }
+    if ( typeof $(this).attr('data-modal-width') != 'undefined' ) {
+      // todo: make for href='#modalId'
+      modal_w.attr('data-width',$(this).attr('data-modal-width'));
+    }
+    if ( typeof $(this).attr('data-modal-min-width') != 'undefined' ) {
+      // todo: make for href='#modalId'
+      modal_w.attr('data-min-width',$(this).attr('data-modal-min-width'));
+    }
+
+    if ( typeof $(this).attr('data-show-loader') != 'undefined' && typeof $(this).attr('href') != 'undefined') {
+      modal_w.find('.modal-body').html('<div class="loader form_loader"></div>');
+    }
+
   });
 
 });
