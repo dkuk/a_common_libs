b4cb414553fd83a5885b65e90b2a994abc6dede6
Alexey Glukhov <gluhov1985@gmail.com>
2014-04-15 15:31:45 +1100

    Added utils function to use select2 with text inputs

--- assets/javascripts/utils.js
+++ assets/javascripts/utils.js
@@ -45,34 +45,79 @@ RMPlus.Utils = (function(my) {
     };
   }
 
-  my.modifyFormForComboboxes = function(event, select2combobox_names){
-    if(Object.prototype.toString.call(select2combobox_names) !== '[object Array]' ) {
-      select2combobox_names = new Array(select2combobox_names);
+  // Function makes select2 combobox out of text field
+  // Accepts jquery selector and init data (as a js array)
+  my.makeSelect2Combobox = function(selector, init_data){
+    var $selector = $(selector);
+    // add combobox flag, if not already present
+    if ($selector.attr('data-combobox') !== "true") {
+      $selector.attr('data-combobox', 'true');
     }
 
-    event.preventDefault();
-    var $this = $(this);
-    var formData = $this.serializeArray();
-    var newFormData = new Array();
-    for (var i = 0, len1 = formData.length; i < len1; i++){
-      if (select2combobox_names.contains(formData[i].name)) {
-        var arr = formData[i].value.split(',');
-        for (var j = 0, len2 = arr.length; j < len2; j++){
-          var formValue = {name: formData[i].name, value: arr[j] };
-          newFormData.push(formValue);
-        }
+    init_data = init_data || $selector.val();
+
+    if( Object.prototype.toString.call(init_data) === '[object String]' ) {
+      if (init_data.length > 0){
+        init_data = init_data.split(',');
       }
       else {
-        newFormData.push(formData[i]);
+        init_data = [];
       }
     }
-    console.log(formData);
-    console.log(newFormData);
-    $.ajax({
-      type: 'POST',
-      url: $this.attr('action'),
-      data: newFormData
+
+    // make data array with objects with id and text properties out of ordinary values array (select2 requirement)
+    var data_select2 = [];
+    for (var i = 0, len = init_data.length; i < len; i++){
+      data_select2[i] = {id: init_data[i], text: init_data[i]};
+    }
+
+    // populate text field with init values
+    if ($selector.val() === "") {
+      $selector.val(init_data);
+    }
+
+    // You can't enter text in select2 textfield without createSearchChoice function defined
+    $selector.select2({
+      createSearchChoice: function(term, data){
+        if ($(data).filter(function() { return this.text.localeCompare(term) === 0; }).length === 0){
+          return {id:term, text:term};
+        }
+      },
+      multiple: true,
+      width: 'resolve',
+      data: data_select2
     });
+  }
+
+  // There is problem with using Select2 in 'data' mode with text inputs to handle arrays.
+  // Select2 keeps all entered items as a concatenated comma-separated string in input "value" attribute
+  // This function catches all such inputs marked with 'data-combobox' flag, and replaces them with selects with options
+  // so that arrays will be sent the right way.
+  my.modifyFormForComboboxes = function(event){
+    //event.preventDefault();
+    var $form = $(this);
+    var form = this;
+
+    $form.find('input[type="text"]').each(function(index){
+      if ($(this).attr('data-combobox') === "true"){
+        var value = $(this).val();
+        var valuesArray = value.split(',');
+        var name = this.name;
+        $(this).remove();
+
+        var $select = $('<select multiple="multiple" tabindex="-1" style="display: none;"></select>');
+        $select.attr('id', name);
+        $select.attr('name', name);
+        for (var i = 0, len = valuesArray.length; i < len; i++){
+          var $option = $('<option selected="selected"></option>');
+          $option.val(valuesArray[i]);
+          $option.appendTo($select);
+        }
+
+        $(form).append($select);
+      }
+    });
+    return true;
   };
 
   return my;
