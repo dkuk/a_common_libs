2170c69eb6b092f6e7ca44bede4189b52881fa93
Alexey Glukhov <gluhov1985@gmail.com>
2014-04-18 15:44:48 +1100

    Added util function to check for existence of nested properties in RMPlus namespace (useful for various settings)

--- assets/javascripts/utils.js
+++ assets/javascripts/utils.js
@@ -12,6 +12,22 @@ var RMPlus = (function (my) {
 RMPlus.Utils = (function(my) {
   var my = my || {};
 
+  // function checks existence of the property in the RMPlus namespace recursively
+  my.exists = function(prop) {
+    obj = RMPlus;
+    var parts = prop.split('.');
+    for(var i = 0, l = parts.length; i < l; i++) {
+      var part = parts[i];
+      if(obj !== null && typeof obj === "object" && part in obj) {
+          obj = obj[part];
+      }
+      else {
+          return false;
+      }
+    }
+    return true;
+  }
+
   // useful functions to decorate autocomplete handlers, etc.
   // see http://habrahabr.ru/post/60957/  (rus),
   // http://drupalmotion.com/article/debounce-and-throttle-visual-explanation (eng) for more info
