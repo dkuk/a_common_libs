b932ff0afa78f6b1d7990f075b1f1de8a187666d
Alexey Glukhov <gluhov1985@gmail.com>
2014-05-13 11:22:15 +1100

    Added assets file listeners in development mode for easy editing of javascript and css files

--- init.rb
+++ init.rb
@@ -12,6 +12,43 @@ end
 Rails.application.config.to_prepare do
   # ApplicationController.send(:include, ACommonLibs::ApplicationControllerPatch)
   ApplicationHelper.send(:include, ACommonLibs::ApplicationHelperPatch)
+
+  if Rails.env.development?
+    $listeners = []
+    Rails.logger.debug "Init Listeners..."
+    Redmine::Plugin.registered_plugins.each do |name, plugin|
+      source = plugin.assets_directory
+      if File.exist?(source) && File.directory?(source)
+        destination = plugin.public_directory
+        assets_listener = Listen.to source do |modified, added, removed|
+          modified.each do |modified_path|
+            if File.file?(modified_path)
+              target = File.join(destination, modified_path.gsub(source, ''))
+              FileUtils.cp(modified_path, target)
+            end
+          end
+          added.each do |added_path|
+            if File.directory?(added_path)
+              FileUtils.mkdir_p(added_path)
+            elsif File.file?(added_path)
+              target = File.join(destination, added_path.gsub(source, ''))
+              FileUtils.cp(added_path, target)
+            end
+          end
+          removed.each do |removed_path|
+            target = File.join(destination, removed_path.gsub(source, ''))
+            FileUtils.remove_entry(target, true)
+          end
+        end
+        assets_listener.start
+        $listeners << assets_listener
+      end
+    end
+    at_exit do
+      Rails.logger.debug "Stopping listeners..."
+      $listeners.each{ |listener| listener.stop }
+    end
+  end
 end
 
 require 'a_common_libs/view_hooks'
