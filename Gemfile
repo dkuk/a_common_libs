09e1cb275ad110882bc8b0cd32e33adb3cfddd8a
Alexey Glukhov <gluhov1985@gmail.com>
2014-05-15 16:26:25 +1100

    Moved some code to rmplus_dev

--- Gemfile
+++ Gemfile
@@ -1,5 +1,1 @@
 source "http://rubygems.org"
-
-group :development do
-  gem 'listen', '~> 2.0'
-end
--- init.rb
+++ init.rb
@@ -9,49 +9,6 @@ Redmine::Plugin.register :a_common_libs do
   settings :partial => 'settings/a_common_libs'
 end
 
-Rails.application.config.after_initialize do
-  if Rails.env.development?
-    enable_assets_listeners = Setting.plugin_a_common_libs[:enable_assets_listeners]
-    if enable_assets_listeners
-      $listeners = []
-      Rails.logger.debug "Initializing listeners..."
-      Redmine::Plugin.registered_plugins.each do |name, plugin|
-        source = plugin.assets_directory
-        if File.exist?(source) && File.directory?(source)
-          destination = plugin.public_directory
-          assets_listener = Listen.to source do |modified, added, removed|
-            modified.each do |modified_path|
-              if File.file?(modified_path)
-                target = File.join(destination, modified_path.gsub(source, ''))
-                FileUtils.cp(modified_path, target)
-              end
-            end
-            added.each do |added_path|
-              if File.directory?(added_path)
-                FileUtils.mkdir_p(added_path)
-              elsif File.file?(added_path)
-                target = File.join(destination, added_path.gsub(source, ''))
-                FileUtils.cp(added_path, target)
-              end
-            end
-            removed.each do |removed_path|
-              target = File.join(destination, removed_path.gsub(source, ''))
-              FileUtils.remove_entry(target, true)
-            end
-          end
-          Rails.logger.debug "Starting assets listener for plugin #{name}"
-          assets_listener.start
-          $listeners << assets_listener
-        end
-      end
-      at_exit do
-        Rails.logger.debug "Stopping listeners..."
-        $listeners.each{ |listener| listener.stop }
-      end
-    end
-  end
-end
-
 Rails.application.config.to_prepare do
   # ApplicationController.send(:include, ACommonLibs::ApplicationControllerPatch)
   ApplicationHelper.send(:include, ACommonLibs::ApplicationHelperPatch)
