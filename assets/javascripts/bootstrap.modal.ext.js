7fbf7f0f6ab3d44bd3a003b6e26089f8ab8959c4
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2014-05-25 21:24:41 +1100

    #20269 total remake modals resize

--- assets/javascripts/bootstrap.modal.ext.js
+++ assets/javascripts/bootstrap.modal.ext.js
@@ -9,160 +9,192 @@ RMPlus.LIB = (function (my) {
 
   my.resize_bs_modal = function (obj) {
     /* handle width */
+    var $bs_modal = $(obj);
     var w_width = $(window).width();
     var w_height = $(window).height();
-    var paddings_w = $(obj).outerWidth() - $(obj).width();
+    var paddings_w = $bs_modal.outerWidth() - $bs_modal.width();
     var px_width;
     var px_height;
     // var max_px_height = 0;
 
-    if ($(obj).attr('data-width') !== undefined) {
-      $(obj).css('margin-left', 0);
-      if ($(obj).attr('data-width').indexOf('%') > -1) {
-        px_width = w_width*parseInt($(obj).attr('data-width'))/100;
+    if ($bs_modal.attr('data-width') != undefined) {
+      $bs_modal.css('margin-left', 0);
+      if ($bs_modal.attr('data-width').indexOf('%') > -1) {
+        px_width = w_width*parseInt($bs_modal.attr('data-width'))/100;
       }
       else {
-        px_width = parseInt($(obj).attr('data-width'));
+        px_width = parseInt($bs_modal.attr('data-width'));
       }
 
       var left = (w_width - px_width - paddings_w) / 2;
-      $(obj).css('left', left);
-      $(obj).css('width', px_width);
+      $bs_modal.css('left', left);
+      $bs_modal.css('width', px_width);
     }
-    else if ($(obj).attr('data-min-width') !== undefined) {
-      $(obj).css('margin-left', 0);
-      $(obj).css('width', 'auto');
+    else if ($bs_modal.attr('data-min-width') !== undefined) {
+      $bs_modal.css('margin-left', 0);
+      $bs_modal.css('width', 'auto');
 
-      $(obj).css('min-width', $(obj).attr('data-min-width'));
-      var left = (w_width - $(obj).outerWidth()) / 2;
-      $(obj).css('left', left);
+      $bs_modal.css('min-width', $bs_modal.attr('data-min-width'));
+      var left = (w_width - $bs_modal.outerWidth()) / 2;
+      $bs_modal.css('left', left);
+    }
+    var $modal_body = $bs_modal.find('.modal-body');
+    var $modal_footer = $bs_modal.find('.modal-footer');
+    var $modal_header = $bs_modal.find('.modal-footer');
+
+    // set initial height
+    if (typeof $bs_modal.attr('data-header-height') != 'undefined' ||
+        typeof $bs_modal.attr('data-body-height') != 'undefined' ||
+        typeof $bs_modal.attr('data-footer-height') != 'undefined' ||
+        typeof $bs_modal.attr('data-full-height') != 'undefined') {
+      $modal_header.height($bs_modal.attr('data-header-height')+'px');
+      $modal_body.height($bs_modal.attr('data-body-height')+'px');
+      $modal_footer.height($bs_modal.attr('data-footer-height')+'px');
+      $bs_modal.height($bs_modal.attr('data-full-height')+'px');
+      $bs_modal.removeAttr('data-header-height');
+      $bs_modal.removeAttr('data-body-height');
+      $bs_modal.removeAttr('data-footer-height');
+      $bs_modal.removeAttr('data-full-height');
+    }
+    else {
+      $modal_header.css('height','');
+      $modal_body.css('height','');
+      $modal_footer.css('height','');
+      $bs_modal.css('height','');
     }
 
 
-    var modal_body = $(obj).find('.modal-body')
-    if ($(obj).attr('data-height') !== undefined) {
+    if ($bs_modal.attr('data-height') != undefined) {
       var ext_height = 0;
-      if ($(obj).find('.modal-footer').length == 1) {
-        ext_height += $(obj).find('.modal-footer').outerHeight()
+      if ($modal_footer.length == 1) {
+        ext_height += $modal_footer.outerHeight()
       }
-      if ($(obj).find('.modal-header').length == 1) {
-        ext_height += $(obj).find('.modal-header').outerHeight()
+      if ($modal_header.length == 1) {
+        ext_height += $modal_header.outerHeight()
       }
-      var paddings_h = modal_body.outerHeight() - modal_body.height();
+      var paddings_h = $modal_body.outerHeight() - $modal_body.height();
 
-      if ($(obj).attr('data-height').indexOf('%') > -1) {
-        px_height = w_height*parseInt($(obj).attr('data-height'))/100;
+      if ($bs_modal.attr('data-height').indexOf('%') > -1) {
+        px_height = w_height*parseInt($bs_modal.attr('data-height'))/100;
       }
       else {
-        px_height = parseInt($(obj).attr('data-height'));
+        px_height = parseInt($bs_modal.attr('data-height'));
       }
 
       var top = (w_height - px_height) / 2;
-      $(obj).css('top', top);
-      modal_body.css('max-height', px_height-ext_height-paddings_h);
+      $bs_modal.css('top', top);
+      $modal_body.css('max-height', px_height-ext_height-paddings_h);
 
 
-      // if(modal_body.height() < modal_height-ext_height) {
+      // if($modal_body.height() < modal_height-ext_height) {
         // проблемы, когда контент в окне изменяется. Нельзя вызвать циклический ресайз!
-      //   var top = (w_height - $(obj).height()) / 2;
+      //   var top = (w_height - $bs_modal.height()) / 2;
       //   // alert('smaller! top='+top)
-      //   $(obj).css('top', top);
+      //   $bs_modal.css('top', top);
       // }
       // else {
-      //   var top = (100 - parseInt($(obj).attr('data-height'))) / 2;
-      //   $(obj).css('top', top.toString()+'%');
+      //   var top = (100 - parseInt($bs_modal.attr('data-height'))) / 2;
+      //   $bs_modal.css('top', top.toString()+'%');
       // }
     }
-    if (typeof $(obj).attr('data-onload-height') != 'undefined') {
-      modal_body.height($(obj).attr('data-onload-height')+'px');
-      $(obj).removeAttr('data-onload-height');
-    }
-    else {
-      modal_body.css('height','');
-    }
+
   } /* resize_bs_modal ENDS */
 
-  my.render_dynamic_bs_modal = function (link_obj) {
-    var modal_w;
-    var m_body = '<div class="modal-body"></div>';
-    var $link = $(link_obj);
-    var cur_bs = $('#bs-modal-dynamic');
-    if ( typeof $link.attr('data-store-parent-height') != 'undefined' && $link.parents('#bs-modal-dynamic').length > 0 && typeof $link.attr('data-modal-load-height') == 'undefined') {
-      $link.attr('data-modal-load-height', cur_bs.find('.modal-body').height());
+  my.store_previous_bs_modal_size_and_hide = function (link) {
+    var $link = $(link);
+    var $cur_bs = $link.parents('.modal').first();
+    if (typeof $cur_bs != 'undefined') {
+      if ( typeof $link.attr('data-save-previous-height') != 'undefined') {
+        $link.attr('data-body-height', $cur_bs.find('.modal-body').height());
+        $link.attr('data-footer-height', $cur_bs.find('.modal-footer').height());
+        $link.attr('data-header-height', $cur_bs.find('.modal-footer').height());
+        $link.attr('data-full-height', $cur_bs.height());
+      }
+      $cur_bs.modal('hide');
+      if($cur_bs.attr('id') == 'bs-modal-dynamic') $cur_bs.remove();
     }
+  }
 
-    cur_bs.modal('hide');
-    cur_bs.remove();
-
-    if (typeof $link.attr('data-iframe') != 'undefined') {
-      m_body = '<iframe class="modal-body modal-iframe" height="825px" frameborder="0" hspace="0" src="'+$link.attr('href')+'" scrolling="auto"></iframe>';
-    }
-    if ( typeof $link.attr('data-target') == 'undefined' || $($link.attr('data-target')).length < 1) {
-      var close_label = (typeof $link.attr('data-close-label') != 'undefined') ? $link.attr('data-close-label') : 'Close';
-      $('<div id="bs-modal-dynamic" class="modal I fade" tabindex="-1" role="dialog" aria-hidden="true" style="z-index: 1061;">'+
-          m_body+
-           '<div class="modal-footer">'+
-             '<button class="" data-dismiss="modal" aria-hidden="true" style="vertical-align: middle;">'+close_label+'</button>'+
-           '</div>'+
-         '</div>').appendTo(document.body);
-
-      modal_w = $('#bs-modal-dynamic');
-      $link.attr('data-target','#bs-modal-dynamic')
-    } else {
-      modal_w = $($link.attr('data-target'));
-    }
-
+  my.fill_bs_modal_attrs = function ($link, $bs_modal) {
     if ( typeof $link.attr('data-modal-height') != 'undefined' ) {
       // todo: make for href='#modalId'
-      modal_w.attr('data-height', $link.attr('data-modal-height'));
+      $bs_modal.attr('data-height', $link.attr('data-modal-height'));
     }
     if ( typeof $link.attr('data-modal-width') != 'undefined' ) {
       // todo: make for href='#modalId'
-      modal_w.attr('data-width', $link.attr('data-modal-width'));
+      $bs_modal.attr('data-width', $link.attr('data-modal-width'));
     }
     if ( typeof $link.attr('data-modal-min-width') != 'undefined' ) {
       // todo: make for href='#modalId'
-      modal_w.attr('data-min-width', $link.attr('data-modal-min-width'));
+      $bs_modal.attr('data-min-width', $link.attr('data-modal-min-width'));
     }
 
-    if ( typeof $link.attr('data-modal-load-height') != 'undefined' ) {
-      modal_w.attr('data-onload-height', $link.attr('data-modal-load-height'));
+    if ( typeof $link.attr('data-body-height') != 'undefined' ) {
+      $bs_modal.attr('data-body-height', $link.attr('data-body-height'));
     }
+    if ( typeof $link.attr('data-footer-height') != 'undefined' ) {
+      $bs_modal.attr('data-footer-height', $link.attr('data-footer-height'));
+    }
+    if ( typeof $link.attr('data-header-height') != 'undefined' ) {
+      $bs_modal.attr('data-header-height', $link.attr('data-header-height'));
+    }
+  }
 
-    if (typeof $link.attr('href') != 'undefined') {
-      if ( typeof $link.attr('data-show-loader') != 'undefined') {
-        modal_w.find('.modal-body').html('<div class="big_loader form_loader"></div>');
+  my.render_dynamic_bs_modal = function (link) {
+
+    var $link = $(link);
+    RMPlus.LIB.store_previous_bs_modal_size_and_hide(link);
+    var $target = $($link.attr('data-target') || ($link.href && $link.href.replace(/.*(?=#[^\s]+$)/, '')));
+
+    // Render dynamic modal
+    if ($target.length == 0) {
+      var m_footer = '<div class="modal-footer">'+
+                       '<button data-dismiss="modal" aria-hidden="true" style="vertical-align: middle;">'+
+                         ((typeof $link.attr('data-close-label') != 'undefined') ? $link.attr('data-close-label') : 'Close')+
+                       '</button>'+
+                     '</div>';
+      $target = $('<div id="bs-modal-dynamic" class="modal I fade" role="dialog" aria-hidden="true" style="z-index: 1061;"></div>');
+      var modal_type = $link.attr('data-modal-type')
+      if (typeof modal_type != 'undefined' && modal_type == 'clear') {
+        // do nothing - content will be load to modal base
+      } else if (typeof modal_type != 'undefined' && (modal_type == 'iframe' || modal_type == 'iframe-only')) {
+        if (modal_type == 'iframe-only') { m_footer = '' }
+        $('<iframe class="modal-body modal-iframe" height="825px" frameborder="0" hspace="0" src="'+$link.attr('href')+'" scrolling="auto"></iframe>'+m_footer).appendTo($target);
       }
-      // for load it self and trigger resize due loaded.bs.modal does not work
-      if (typeof $link.attr('data-load-self') != 'undefined') {
-        modal_w.find('.modal-body').load(link_obj.href, function () {
+      else {
+        // default bootsrap modal - load nothing, only sizes
+        $('<div class="modal-body"></div>'+m_footer).appendTo($target);
+      }
+
+      $target.appendTo(document.body);
+      RMPlus.LIB.fill_bs_modal_attrs($link, $target);
+
+      if (typeof modal_type != 'undefined' && (modal_type == 'iframe' || modal_type == 'iframe-only')) {
+        $target.modal('show');
+        return false;
+      } else {
+        // load data self and trigger resize due loaded only sciense 3rd Bottstrap
+        var $load_to = $target.find('.modal-body');
+        if ($load_to.length == 0) { $load_to = $target }
+        if ($link.hasClass('show-loader')) {
+          $load_to.html('<div class="big_loader form_loader"></div>');
+        }
+        // for load it self and trigger resize due loaded.bs.modal does not work
+        $load_to.load(link.href, function () {
           $('.tabs-buttons').hide();
-          RMPlus.LIB.resize_bs_modal(modal_w.get(0));
+          RMPlus.LIB.resize_bs_modal($target.get(0));
         });
-        modal_w.modal('show');
+        $target.modal('show');
         return false;
       }
-    }
-    if (typeof $link.attr('data-iframe') != 'undefined') {
-      modal_w.modal('show');
-      return false;
+    } else {
+      // standart BS Modal behaviour with sizes
+      // skip show, load etc. leave this actions to BS javascript
+      RMPlus.LIB.fill_bs_modal_attrs($link, $target);
     }
     return true;
-  }
 
-  my.load_bs_ajax_form = function (link) {
-    var form_div = $('#'+$(link).attr('data-target'));
-    if (form_div.length > 0) {
-      form_div.html('<div class="loader form_loader"></div>');
-      form_div.modal('show');
-      form_div.load(link.href, function () {
-          $('.tabs-buttons').hide();
-          RMPlus.LIB.resize_bs_modal(form_div);
-      });
-    }
-    return false;
   }
-
 
   return my;
 })(RMPlus.LIB || {});
@@ -189,10 +221,6 @@ $(document).ready(function () {
     $('.modal').each(function () {
       RMPlus.LIB.resize_bs_modal(this);
     });
-  });
-
-  $(document.body).on('click', '.bs-ajax-form', function () {
-    return RMPlus.LIB.load_bs_ajax_form(this);
   });
 
   $(document.body).on('click','[data-toggle=modal]', function () {
