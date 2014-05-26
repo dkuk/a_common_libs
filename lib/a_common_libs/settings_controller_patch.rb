b0c8bc2b5e5618a213cae2ac720bc3316b193a17
Alexey Glukhov <gluhov1985@gmail.com>
2014-05-26 14:25:26 +1100

    Fixed redmine bug where entire application fails when trying to access plugin setting, if all plugin settings were reset. With all settings reset there is no params[:setting] in the POST request to settings controller, calling params[:setting].to_s caused empty string to be saved in place of the empty hash. When accessing settings property empty string is evaluated to empty array [], thus the error 'Can't convert Symbol to Integer'

---
+++ lib/a_common_libs/settings_controller_patch.rb
@@ -0,0 +1,29 @@
+module ACommonLibs
+  module SettingsControllerPatch
+
+    def self.included(base)
+
+      base.extend(ClassMethods)
+      base.send(:include, InstanceMethods)
+
+      base.class_eval do
+        alias_method_chain :plugin, :a_common_libs
+      end
+    end
+
+    module ClassMethods
+    end
+
+    module InstanceMethods
+      def plugin_with_a_common_libs
+        if request.post?
+          if params[:settings].nil? || params[:settings] == ''
+            params[:settings] = HashWithIndifferentAccess.new
+          end
+        end
+        plugin_without_a_common_libs
+      end
+    end
+
+  end
+end
